02ab4468402e761812e4b04bbda54bac84b54988
venkin84 <venkin84@Venkataramans-MacBook-Pro.local>
2016-07-08 12:13:50 +0530

    - Created tournament database with tables players and matches

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
 
 
-def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+def connect(psql_dbName, psql_user, psql_host, psql_password):
+    try:
+        pg_connection = psycopg2.connect(dbname=psql_dbName, user=psql_user, host=psql_host, password=psql_password)
+        return pg_connection;
+    except psycopg2.Error as e:
+        print e.pgerror
+        return None;
 
 
 def deleteMatches():
@@ -25,10 +29,10 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -56,16 +60,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +77,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,21 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE tournament;
 
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE players(
+    playerID SERIAL PRIMARY KEY,
+    playerName varchar(80) NOT NULL,
+    wins integer DEFAULT 0,
+    matches integer DEFAULT 0
+);
+
+CREATE TABLE matches(
+    matchID SERIAL PRIMARY KEY,
+    winnerID integer REFERENCES players (playerID),
+    looserID integer REFERENCES players (playerID)
+);
